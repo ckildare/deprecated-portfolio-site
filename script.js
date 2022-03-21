1918b2d9f4e790f236898576ce36e4a171ea4d8a
Connor Kildare <ckildare10@gmail.com>
2022-03-20 21:28:50 -0500

    Commented style.scss and expereimented with dropdowns for the project cards.

---
+++ script.js
@@ -0,0 +1,17 @@
+function dropdown() {
+    document.getElementById("project-dropdown").classList.toggle("show");
+}
+  
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("project-content");
+        var iter;
+        for (iter = 0; i < dropdowns.length; iter++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
